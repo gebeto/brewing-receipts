5eecc1162d902594562d849a2578eddc1cf85ddd
gebeto <slavik.nychkalo@gmail.com>
2025-01-10 19:55:16 +0200

    add animated icons

--- src/Step.tsx
+++ src/Step.tsx
@@ -1,10 +1,13 @@
-import { motion } from "motion/react";
+import { motion, cubicBezier } from "motion/react";
 import {
   StepDefinitionPoor,
   StepDefinitionWait,
   StepDefinition,
 } from "./receipts";
 import { Timer } from "./Timer";
+
+import goosneckSrc from "./assets/goosneck.png";
+import waitSrc from "./assets/wait.png";
 
 export type StepProps<T extends StepDefinition> = T & {
   active: boolean;
@@ -17,6 +20,19 @@ export const StepWait = (props: StepProps<StepDefinitionWait>) => {
   return (
     <div>
       <motion.h2>Wait</motion.h2>
+      <motion.img
+        src={waitSrc}
+        alt="wait"
+        width="90px"
+        initial={{ scale: 1 }}
+        animate={props.active && { scale: 1.1 }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          ease: "easeInOut",
+          duration: 1,
+        }}
+      />
       <motion.h2 style={{ fontSize: 60, fontFamily: "monospace" }}>
         <Timer
           seconds={props.seconds}
@@ -38,6 +54,19 @@ export const StepPoor = (props: StepProps<StepDefinitionPoor>) => {
   return (
     <div>
       <motion.h2>Poor {props.volume}ml</motion.h2>
+      <motion.img
+        src={goosneckSrc}
+        alt="poor"
+        width="180px"
+        initial={{ rotateZ: 10 }}
+        animate={props.active && { rotateZ: -10 }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          ease: "easeInOut",
+          duration: 1.4,
+        }}
+      />
       <motion.h2 style={{ fontSize: 60 }}>
         up to {props.generalVolume + props.volume}ml
       </motion.h2>
@@ -45,6 +74,9 @@ export const StepPoor = (props: StepProps<StepDefinitionPoor>) => {
         <Timer
           seconds={Math.ceil(props.volume / props.flowRate)}
           active={props.active}
+          beeps={{
+            0: "long",
+          }}
         />
       </motion.h2>
     </div>
