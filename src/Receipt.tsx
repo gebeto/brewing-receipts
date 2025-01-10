e9e428d69f0190a51033ef18ade5a724156f4b03
gebeto <slavik.nychkalo@gmail.com>
2025-01-10 23:42:55 +0200

    improve cards

--- src/Receipt.tsx
+++ src/Receipt.tsx
@@ -5,17 +5,19 @@ import { ReceiptDefinition, receipts } from "./receipts";
 import { Step } from "./Step";
 import { useNavigate, useParams } from "react-router";
 
-const StepperRoot = styled(motion.div)`
+const ReceiptRoot = styled(motion.div)`
   display: flex;
   justify-content: flex-start;
   flex-direction: column;
   flex: 1;
   width: 100%;
   border-radius: 16px;
+  overflow-x: hidden;
 `;
 
 const StepRoot = styled(motion.div)`
   color: #000;
+  background-color: #fff;
   border-radius: 16px;
 
   min-height: 60vh;
@@ -59,6 +61,7 @@ const Header: React.FC<{ receipt: ReceiptDefinition }> = ({ receipt }) => {
         alignItems: "center",
         gap: 16,
         borderBottom: "1px solid #ddd",
+        backgroundColor: "#fff",
         padding: 16,
       }}
     >
@@ -87,7 +90,7 @@ const StartStep: React.FC<{
       }}
       initial={{ scale: 1 }}
       animate={{ scale: 1 }}
-      exit={{ scale: 0, opacity: 0 }}
+      exit={{ scale: 0.8, opacity: 0.4 }}
     >
       <motion.button onClick={onStart}>START</motion.button>
     </motion.div>
@@ -124,81 +127,64 @@ export const Receipt = () => {
   const receiptId = useParams<string>().id ?? "0";
   const receipt = receipts[parseInt(receiptId)];
 
+  const step = receipt.steps[currentStep];
+  const prevSteps = receipt.steps.slice(0, currentStep);
+  const generalVolume = prevSteps.reduce((acc, step) => {
+    if (step.type === "poor") {
+      return acc + step.volume;
+    }
+    return acc;
+  }, 0);
+
   return (
-    <StepperRoot>
+    <ReceiptRoot>
       <Header receipt={receipt} />
-      <AnimatePresence mode="popLayout">
+      <motion.div style={{ padding: 16 }}>
         {currentStep === -1 && (
-          <StartStep onStart={() => setStep((count) => count + 1)} />
+          <StartStep
+            key={currentStep}
+            onStart={() => setStep((count) => count + 1)}
+          />
         )}
-        <motion.div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 8,
-            padding: 16,
-          }}
-        >
-          {receipt.steps.map((step, index, arr) => {
-            if (currentStep > index) {
-              return null;
-            }
-
-            const prevSteps = arr.slice(0, index);
-            const generalVolume = prevSteps.reduce((acc, step) => {
-              if (step.type === "poor") {
-                return acc + step.volume;
-              }
-              return acc;
-            }, 0);
-
-            return (
-              <StepRoot
-                layout
-                key={index}
-                exit={{ scale: 0.8, opacity: 0 }}
-                variants={{
-                  focused: { scale: 1, opacity: 1.0 },
-                  unfocused: { scale: 0.8, opacity: 0.4 },
+        <AnimatePresence mode="popLayout">
+          {step && (
+            <StepRoot
+              layout
+              key={currentStep}
+              initial={{ scale: 0.96, x: 130, opacity: 0 }}
+              animate={{ scale: 1, x: 0, opacity: 1 }}
+              exit={{ scale: 0.96, x: -130, opacity: 0 }}
+            >
+              <motion.div style={{ padding: 16 }}>
+                <Step
+                  step={step}
+                  active={true}
+                  generalVolume={generalVolume}
+                  onNext={() => setStep((count) => count + 1)}
+                  onBack={() => setStep((count) => count - 1)}
+                />
+              </motion.div>
+              <motion.div style={{ flex: 1 }} />
+              <motion.div
+                style={{
+                  padding: 16,
+                  borderTop: "1px solid #ddd",
                 }}
-                initial={currentStep === index ? "focused" : "unfocused"}
-                animate={currentStep === index ? "focused" : "unfocused"}
               >
-                <motion.div style={{ padding: 16 }}>
-                  <Step
-                    step={step}
-                    active={currentStep === index}
-                    generalVolume={generalVolume}
-                    onNext={() => setStep((count) => count + 1)}
-                    onBack={() => setStep((count) => count - 1)}
-                  />
-                </motion.div>
-                <motion.div style={{ flex: 1 }} />
-                {index === currentStep && (
-                  <motion.div
-                    style={{
-                      padding: 16,
-                      borderTop: "1px solid #ddd",
-                    }}
-                  >
-                    <motion.button
-                      onClick={() => setStep((count) => count + 1)}
-                      initial={{ scale: 0 }}
-                      animate={{ scale: 1 }}
-                      style={{ width: "100%" }}
-                    >
-                      Next
-                    </motion.button>
-                  </motion.div>
-                )}
-              </StepRoot>
-            );
-          })}
-        </motion.div>
-        {currentStep === receipt.steps.length && (
-          <EndStep onEnd={() => setStep(-1)} />
-        )}
-      </AnimatePresence>
-    </StepperRoot>
+                <motion.button
+                  onClick={() => setStep((count) => count + 1)}
+                  style={{ width: "100%" }}
+                >
+                  Next
+                </motion.button>
+              </motion.div>
+            </StepRoot>
+          )}
+          {currentStep === receipt.steps.length && (
+            <EndStep onEnd={() => setStep(-1)} />
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </ReceiptRoot>
   );
 };
